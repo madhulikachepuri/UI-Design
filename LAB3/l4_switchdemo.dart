635525aef0b05fab13c1eeafd730a066bf46e5b1
madhulikachepuri <madhulikachepuri@gmail.com>
2025-03-13 13:34:10 +0530

    Add files via upload

--- LAB3/l4_switchdemo.dart
+++ LAB3/l4_switchdemo.dart
@@ -1,12 +1,30 @@
-class l4_switchdemo{
-	void fruits(String c){
-		var x = switch(c){
-			'red' || 'Red' || 'RED' =>c,
-			_=>'Red is a color',
+//class l4_switchdemo{
+	//void fruits(String c){
+		//var x = switch(c){
+			//'red' || 'Red' || 'RED' =>c,
+			//_=>'Red is a color',
 			
-			};
-print(x);
-	}
+			//};
+//print(x);
+	//}
+//}
+//lib
+class l4_switchdemo{
+void sdemo(String name){
+	switch(name){
+		case 'apple' ||'Apple' || 'APPLE':
+			print('this is apple');
+			break;
+		case 'banana' ||'Banana' || 'BANANA':
+			print('this is banana');
+			break;
+		case 'carrot' ||'Carrot' || 'CARROT':
+			print('this is carrot');
+			break;
+		case_:
+			print('invaild not in the list');
+			}
+		}
 }
-
+			
 
